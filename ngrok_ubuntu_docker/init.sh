c746d31d9ce28b473da8d385387203608cd3409b
rodion.ugarov@sharp-dev.net <rodion.ugarov@sharp-dev.net>
2023-09-11 15:17:33 +0400

    fix ngrok install + jq apt install

--- ngrok_ubuntu_docker/init.sh
+++ ngrok_ubuntu_docker/init.sh
@@ -3,9 +3,9 @@
 ngrok_location=$(whereis ngrok)
 if [ "$ngrok_location" == "ngrok:" ]; then
   echo Ngrok install
-  wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-s390x.tgz
-  tar xvzf ./ngrok-v3-stable-linux-s390x.tgz -C /usr/local/bin
-
+  curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+  echo jq install
+  sudo apt install jq
 else
   echo Ngrok is installed
   echo Run \"./build.sh\" and \"./run.sh\" to start tunneled container
